af7c093ee67cf827a23ed086dc731d955b42b75f
Kiryl Zhuk <kizhofficial@gmail.com>
2022-04-07 23:00:45 +0200

    Added Simple endpoints

---
+++ src/main/java/com/dojo/simpleapi/Controller.java
@@ -0,0 +1,30 @@
+package com.dojo.simpleapi;
+import io.chucknorris.client.ChuckNorrisClient;
+import io.chucknorris.client.Joke;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class Controller {
+    private final static ChuckNorrisClient client = new ChuckNorrisClient();
+
+    @GetMapping("/joke")
+    public ResponseEntity<String> getMeAJoke() {
+        Joke joke = client.getRandomJoke();
+        return ResponseEntity.ok().body(joke.getValue());
+    }
+    @GetMapping("/jokes/{keyword}")
+    public ResponseEntity<List<String>> getMeSomeJokes(@PathVariable String keyword) {
+        List<Joke> jokes = client.searchJokes(keyword);
+        List<String> jokeText = new ArrayList<>();
+        for(Joke joke : jokes){
+            jokeText.add(joke.getValue());
+        }
+        return ResponseEntity.ok().body(jokeText);
+    }
+
+}
