d149a858c8704b278d23f50042e80b965b34455a
Kiryl Zhuk <kizhofficial@gmail.com>
2022-04-11 10:21:36 +0200

    c

--- src/main/java/com/dojo/simpleapi/controller/JokeController.java
+++ src/main/java/com/dojo/simpleapi/controller/JokeController.java
@@ -1,33 +1,28 @@
-package com.dojo.simpleapi;
+package com.dojo.simpleapi.controller;
 import io.chucknorris.client.ChuckNorrisClient;
 import io.chucknorris.client.Joke;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class JokeController {
     private final static ChuckNorrisClient client = new ChuckNorrisClient();
 
     @CrossOrigin
     @GetMapping("/joke")
-    public ResponseEntity<String> getMeAJoke() {
+    public ResponseEntity<Joke> getMeAJoke() {
         Joke joke = client.getRandomJoke();
-        return ResponseEntity.ok().body(joke.getValue());
+        return ResponseEntity.ok().body(joke);
     }
 
     @CrossOrigin
     @GetMapping("/jokes/{keyword}")
-    public ResponseEntity<List<String>> getMeSomeJokes(@PathVariable String keyword) {
+    public ResponseEntity<List<Joke>> getMeSomeJokes(@PathVariable String keyword) {
         List<Joke> jokes = client.searchJokes(keyword);
-        List<String> jokeText = new ArrayList<>();
-        for(Joke joke : jokes){
-            jokeText.add(joke.getValue());
-        }
-        return ResponseEntity.ok().body(jokeText);
+        return ResponseEntity.ok().body(jokes);
     }
 
 }
---
+++ src/main/java/com/dojo/simpleapi/model/Joke.java
@@ -0,0 +1,12 @@
+package com.dojo.simpleapi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Joke {
+
+    private String value;
+
+}
