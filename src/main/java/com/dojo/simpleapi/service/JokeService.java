dfcc7d351ea164132fb8ce67bf7c7d340bd716c7
Kiryl Zhuk <kizhofficial@gmail.com>
2022-04-11 11:09:19 +0200

    annotation

--- src/main/java/com/dojo/simpleapi/service/JokeService.java
+++ src/main/java/com/dojo/simpleapi/service/JokeService.java
@@ -3,12 +3,14 @@ package com.dojo.simpleapi.service;
 import com.dojo.simpleapi.model.JokeModel;
 import io.chucknorris.client.ChuckNorrisClient;
 import io.chucknorris.client.Joke;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class JokeService {
     private final static ChuckNorrisClient client = new ChuckNorrisClient();
 
